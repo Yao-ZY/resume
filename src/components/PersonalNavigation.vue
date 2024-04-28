3800c989a1cc651c95244b44a10bdb30927dc51e
Yao <zhangyao.jk@gmail.com>
2024-04-28 18:33:11 +1000

    Compeleted PersonalNavigation design

--- src/components/PersonalNavigation.vue
+++ src/components/PersonalNavigation.vue
@@ -1,16 +1,57 @@
 <template>
     <div class = 'navigation'>
-        <div class = 'image'>
-
-        </div>
+        <el-menu class = 'navigation' :default-active="active">
+          <a href="javascript:void(0)" @click="goAnchor('introduce')">
+            <el-menu-item index="1">
+                <i class="el-icon-s-home"></i>
+            </el-menu-item>
+          </a>
+          <a href="javascript:void(0)" @click="goAnchor('aboutMe')">
+            <el-menu-item index="2">
+                <i class="el-icon-user"></i>
+            </el-menu-item>
+          </a>
+          <a href="javascript:void(0)" @click="goAnchor('work')">
+            <el-menu-item index="3">
+                <i class="el-icon-folder-opened"></i>
+            </el-menu-item>
+          </a>
+          <a href="javascript:void(0)" @click="goAnchor('education')">
+            <el-menu-item index="4">
+              <i class="el-icon-school"></i>
+            </el-menu-item>
+          </a> 
+        </el-menu>
     </div>
 </template>
 
 <script>
+export default {
+  props: {
+    active: String,
+    require: true
+  },
+  data () {
+    return {
+      offsetTop: 0
+    }
+  },
+  methods: {
+    goAnchor(id) {
+      var anchor = document.getElementById(id);
+      var detail = document.getElementById("detail");
+      // chrome
+      detail.scrollTop = anchor.offsetTop;
+      // firefox
+      detail.scrollTop = anchor.offsetTop;
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
 @import '../assets/less/theme_color.less';
+
 .navigation {
     width: 80%;
     height: 100%;
@@ -18,5 +59,17 @@
     flex-direction: column;
     background-color: @theme-component-color;
     border-radius: 15px;
+    border: none;
+
+    .el-menu-item.is-active, .el-menu-item:hover {
+        color: @theme-color !important;
+        background-color: transparent !important;
+    }
+
+    .el-menu-item {
+        background-color: transparent !important;
+        margin-left: -5px;
+    }
+
 }
 </style>
