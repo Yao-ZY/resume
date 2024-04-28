b91bfe743e9c0a1f27a7f6898c06abb9da0bc10b
Yao <zhangyao.jk@gmail.com>
2024-04-28 15:56:42 +1000

    Compeleted PersonalDetail part

--- src/components/PersonalDetail.vue
+++ src/components/PersonalDetail.vue
@@ -1,10 +1,209 @@
 <template>
     <div class = 'detail'>
         <div class = 'content' style="margin-top: 48px">
-
+            <el-button class="tag" round icon="el-icon-s-home" style="color: #1DA56C; font-weight: 800;">
+                INTRODUCE
+            </el-button>
+            <div class = 'title'>
+                I Craft The <strong style="color: #1DA56C">Digital Landscape </strong>
+            </div>
+            <div class = 'introduce'>
+                I am a  <strong>Frontend Developer </strong>. heart and i create features that are best suited for the job at hand. 
+            </div>
+            <div class = "require_item">
+                <img src="../assets/images/icon_require.png" alt="require">
+                Available for work
+            </div>
+            <div class = "require_item">
+                <img src="../assets/images/icon_require.png" alt="require">
+                Part Time Job
+            </div>
+            <el-button class = 'button' type="primary" round>
+                    HIRE ME
+            </el-button>
+            <div class = "inline_block"></div>
         </div>
         <div class = 'content'>
-
+            <el-button class="tag" round icon="el-icon-user-solid" style="color: #1DA56C; font-weight: 800;">
+                ABOUT ME
+            </el-button>
+            <div class = 'title'>
+                About <strong style="color: #1DA56C">Me </strong>
+            </div>
+            <div class = 'introduce'>
+                Hi, my name is <strong>Yao Zhang </strong>. To seek and maintain full-time position that offers professional
+                challenges utilizing interpersonal skills, excellent time management
+                and problem-solving skills. 
+            </div>
+            <div class = "tags">
+                <p class = 'tags_item'> HTML5 & CSS3(94%)</p>
+                <p class = 'tags_item'> JAVASCRIPT(94%)</p>
+                <p class = 'tags_item'> VUE(94%)</p>
+                <p class = 'tags_item'> ECHARTS(60%)</p>
+            </div>
+            <div class = 'contact'>
+                <div class = 'contact_line'>
+                    <div class = 'language'>
+                        <div class = 'contact_title'>
+                            Language:
+                        </div>
+                        <div class = 'contact_content'>
+                            English, Mandarin
+                        </div>
+                    </div>
+                    <div class = 'phone'>
+                        <div class = 'contact_title'>
+                            Phone:
+                        </div>
+                        <div class = 'contact_content'>
+                            (+61) 0432911368
+                        </div>
+                    </div>
+                </div>
+                <div class = 'contact_line'>
+                    <div class = 'email'>
+                        <div class = 'contact_title'>
+                            Email:
+                        </div>
+                        <div class = 'contact_content'>
+                            zhangyao.jk@gmail.com
+                        </div>
+                    </div>
+                    <div class = 'github'>
+                        <div class = 'contact_title'>
+                            GitHub:
+                        </div>
+                        <div class = 'contact_content'>
+                            https://github.com/Yao-ZY
+                        </div>
+                    </div> 
+                </div>
+            </div>
+            <div class = 'experience'>
+                <div class = "experience_item">
+                    <div class = 'experience_no'>2+</div>  
+                    <div class = 'experience_content'>Years Of Experience </div>
+                </div>
+                <div class = "experience_item">
+                    <div class = 'experience_no'>4+</div>  
+                    <div class = 'experience_content'>Handled Projects </div>
+                </div>
+            </div>
+            <div class = "inline_block"></div>
+        </div>
+        <div class = 'content'>
+            <el-button class="tag" round icon="el-icon-folder-opened" style="width:14%; height: 6%;color: #1DA56C; font-weight: 800;">
+                RESUME
+            </el-button>
+            <div class = 'title'>
+                Work <strong style="color: #1DA56C">Experience </strong>
+            </div>
+            <div class = 'work'>
+                <div class = 'work_career'>
+                    <div class = 'work_career_company'>
+                            Beijing Momo Technology Co., Ltd
+                        </div>
+                    <div class = 'work_career_1'>
+                        <div class = 'work_career_name'>
+                            Front-end Development Intern - Beijing City, China
+                        </div>
+                        <div class = 'work_career_date'>
+                            04/2021 - 06/2022
+                        </div>
+                    </div>
+                    <div class = 'work_carrer_2'>
+                        <div class = 'work_career_name'>
+                            Front-end Development Engineer - Beijing City, China
+                        </div>
+                        <div class = 'work_career_date'>
+                            06/2022 - 03/2023
+                        </div>
+                    </div>
+                </div>
+                <div class = 'work_line'>
+                    <div class = 'work_line_dot'></div>
+                    <div class = 'work_line_dot'></div>
+                </div>
+                <div class = 'work_detail'>
+                    <p>
+                        1. Participated in the development of Argo-web version: handled the
+                        display of design drafts (comparable to sketch design drafts), and
+                        displayed and highlighted the code generation part of the code.
+                    </p>
+                    <p>
+                        2. Participated in the development of control panels: whether
+                        elements are elastic/fixed, absolute layout, style switching, and
+                        whether to show/hide. 
+                    </p>
+                   <p>
+                        3. Independently completed the implementation of Vue componentization 
+                        and parameter extraction.               
+                   </p>
+                   <p>
+                        4. Independently completed the implementation of the React code
+                        generation part.
+                   </p>
+                    <p>
+                        5. Independently completed the refactoring work of the code
+                        generation part.   
+                    </p>
+                    <p>
+                        6. Independently completed the refactoring work of the format
+                        module.  
+                    </p>
+                    <p>
+                        7. Participated in the development of the Gugu App, serving as an
+                        Android developer
+                    </p>
+                    
+                </div>
+            </div>
+            <div class = 'title'>
+                Education <strong style="color: #1DA56C">Experience </strong>
+            </div>
+            <div class = 'education'>
+                <div class = 'education_university'>
+                    <div class = 'education_university_1'>
+                        <div class = 'education_university_name'>
+                            North University of China
+                        </div>
+                        <div class = 'education_university_date'>
+                            09/2018 - 06/2022
+                        </div>
+                    </div>
+                    <div class = 'education_university_2'>
+                        <div class = 'education_university_name'>
+                            Monash University
+                        </div>
+                        <div class = 'education_university_date'>
+                            02/2024 - 12/2025
+                        </div>
+                    </div>
+                </div>
+                <div class = 'education_line'>
+                    <div class = 'education_line_dot'></div>
+                    <div class = 'education_line_dot'></div>
+                </div>
+                <div class = 'education_major'>
+                    <div class = 'education_major_1'>
+                        <div class = 'education_major_degree'>
+                            Bachelor of Science
+                        </div>
+                        <div class = 'education_major_name'>
+                            Computer Software Engineer,
+                        </div>
+                    </div>
+                    <div class = 'education_major_2'>
+                        <div class = 'education_major_degree'>
+                            Master of Science
+                        </div>
+                        <div class = 'education_major_name'>
+                            Information Technology
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class = "inline_block"></div>
         </div>
         <div class = "block"></div>
     </div>
@@ -23,11 +222,284 @@
     flex-direction: column;
 
     .content {
-        width: 100%;
-        min-height: 60%;
+        min-width: 60%;
+        min-height: auto;
         margin-top: 20px;
         background-color: @theme-component-color;
         border-radius: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+
+        .tag {
+            width: 18%;
+            height: 9%;
+            margin-top:5%;
+            margin-left: 8%;
+        }
+
+        .title {
+            font-size: 24px;
+            margin-top:4%;
+            margin-left: 10%;
+        }
+
+        .introduce {
+            display: block;
+            width: 700px;
+            font-size: 16px;
+            font-weight: 500;
+            color: #636363;
+            text-align: start;
+            margin-top:3%;
+            margin-left: 10%;
+            margin-right: 10%;
+            margin-bottom: 2%;
+        }
+
+        .require_item {
+            font-size: 14px;
+            font-weight: 400;
+            color: #636363;
+            margin-top:2%;
+            margin-left: 10%;
+            margin-right: 10%;
+        }
+
+        .button {
+            width: 15%;
+            background-color: @theme-color;
+            font-weight: 700;
+            margin-top: 5%;
+            margin-left: 8%;
+        }
+
+        .tags {
+            font-size: 12px;
+            font-weight: 600;
+            color: #636363;
+            margin-top:2%;
+            margin-left: 8%;
+            margin-right: 8%;
+            display: flex;
+            flex-direction: row;
+            flex-flow: wrap;
+
+            .tags_item {
+                border: 1px dotted #dddddd;
+                border-radius: 20px;
+                padding:10px;
+                margin-left:10px;
+            }
+        }
+
+        .contact {
+            width: 95%;
+            font-size: 14px;
+            font-weight: 600;
+            margin-left:10%;
+            margin-top:2%;
+            
+            .contact_line {
+                display: flex;
+                flex-direction: row;
+                margin-top: 2%;
+            }
+            
+    
+            .language, .phone, .email, .github {  
+                display: flex;
+                flex-direction: row;   
+                justify-content: space-between; 
+                text-align: left;
+
+                .contact_title {
+                    width: 100px;
+                    color: #636363;
+                }
+
+                .contact_content {
+                    width: 200px;
+                    color: #000;
+                  
+                }     
+            }
+        }
+
+        .experience {
+            margin-top: 5%;
+            margin-left: 10%;
+            display: flex;
+            flex-direction: row;
+            
+            .experience_item {
+                display: flex; 
+                flex-direction: column;
+                text-align: left;
+            }
+
+            .experience_item:last-child {
+                margin-left: 50px;
+            }
+
+            .experience_no {
+                color: @theme-color;
+                font-size: 30px;
+            }
+
+            .experience_content {
+                color: #636363;
+                font-size: 14px;
+            }
+        }
+
+        .work {
+            width: 95%;
+            margin-left:10%;
+            margin-top:3%;
+            font-size: 12px;
+            font-weight: 600;
+            color: #636363;
+            display: flex;
+            flex-direction: row;
+            text-align: left;
+
+            .work_career .work_line .work_detail{
+                display: flex;
+                flex-direction: column;
+                flex-wrap: wrap;
+            } 
+
+            .work_career_company {
+                font-size: 14px;
+            }
+
+            .work_career_date {
+                margin-top: 5px;
+            }
+
+            .work_line {
+                width: 1px;
+                height: 95%;
+                background-color: #E2E2E2;
+                margin-left: 10px;
+
+                .work_line_dot {
+                    width: 5px;
+                    height: 5px;
+                    border-radius: 20px;
+                    background-color: @theme-color;
+                    margin-left: -150%;
+                }
+
+                .work_line_dot:first-child {
+                    margin-top: 40px;
+                }
+
+                .work_line_dot:last-child {
+                    margin-top: 100px;
+                }
+            }
+
+            .work_career {
+                width: 300px;
+
+                .work_career_1 {
+                    margin-top: 20px;
+                }
+
+                .work_carrer_2 {
+                    margin-top: 80px;
+                }
+
+                .work_career_date {
+                    color: @theme-color;
+                }
+            }
+
+            .work_detail {
+                margin-left: 20px;
+                width: 50%;
+            }
+        }
+
+        .education {
+            width: 95%;
+            margin-left:10%;
+            margin-top:3%;
+            font-size: 12px;
+            font-weight: 600;
+            color: #636363;
+            display: flex;
+            flex-direction: row;
+            text-align: left;
+
+            .education_university .education_line .education_major{
+                display: flex;
+                flex-direction: column;
+                flex-wrap: wrap;
+            } 
+
+            .education_university_date, .education_major_name {
+                margin-top: 5px;
+            }
+
+            .education_line {
+                width: 1px;
+                height: 100%;
+                background-color: #E2E2E2;
+                margin-left: 10px;
+
+                .education_line_dot {
+                    width: 5px;
+                    height: 5px;
+                    border-radius: 20px;
+                    background-color: @theme-color;
+                    margin-left: -150%;
+                }
+
+                .education_line_dot:first-child {
+                    margin-top: 25px;
+                }
+
+                .education_line_dot:last-child {
+                    margin-top: 60px;
+                }
+            }
+
+            .education_university {
+                width: 300px;
+
+                .education_university_1 {
+                    margin-top: 20px;
+                }
+
+                .education_university_2 {
+                    margin-top: 30px;
+                }
+
+                .education_university_date {
+                    color: @theme-color;
+                }
+            }
+
+            .education_major {
+                margin-left: 20px;
+                width: 50%;
+
+                .education_major_1 {
+                    margin-top: 20px;
+                }
+
+                .education_major_2 {
+                    margin-top: 30px;
+                }
+            }
+        }
+    }
+
+    .inline_block {
+        height: 50px;
     }
 
     .block {
